6c92ed935ccfabbb881623e5ca2012e973f444f4
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-07-11 17:57:13 -0400

    restarted project. Created initial files

---
+++ liri.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+var axios = require("axios")
+
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+
+switch(command) {
+case "concert-this":
+break;
+
+case "spotify-this-song":
+break;
+
+case "movie-this":
+break;
+
+case "do-what-it-says":
+break;
+
+}
